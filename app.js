ab9e45240938c584fe45b13f347aaca91c3c0da9
MelatoninJr <dillon32300@gmail.com>
2023-02-02 22:19:21 -0600

    saving

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 const path = require("path");
-const session = require("express-session");
+var session = require('cookie-session');
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
